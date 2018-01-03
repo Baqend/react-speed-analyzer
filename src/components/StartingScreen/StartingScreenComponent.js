893d3c5aeef76faaf7cbfde2e4c394518437e9df
Julian Tiemann <jutiemann@gmail.com>
2018-01-03 10:11:17 +0100

    add statusCarousel component

--- src/components/StartingScreen/StartingScreenComponent.js
+++ src/components/StartingScreen/StartingScreenComponent.js
@@ -2,33 +2,27 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import ConfigForm from './ConfigForm/ConfigForm'
-import StatusCarousel from './StatusCarousel/StatusCarousel'
+import { StatusCarousel, StatusPage } from './StatusCarousel/StatusCarousel'
 
 class StartingScreenComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      items: [],
-      headline: "We are running a series of tests against your site",
-      details: "See how fast your current backend stack delivers your site to users. We will compare the results to a version of your site using Baqend Speed Kit"
+      status: 0,
     }
   }
 
   componentDidMount() {
-    // this.setState({ items: this.props.children })
-    const items = [
-      <h2 className="text__headline">{this.state.headline}</h2>,
-      <div className="text__details">{this.state.details}</div>
-    ]
-    this.setState({ items: items })
+    setTimeout(() => this.setState({ showCarousel: true }), 2000)
     setInterval(() => {
-      if (!this.state.items.length) {
-        this.setState({ items: items })
+      if (this.state.status === 0) {
+        this.setState({ status: 1 })
       } else {
-        this.setState({ items: [] })
+        this.setState({ status: 0 })
       }
     }, 10000)
   }
+
 
   render() {
     return (
@@ -39,7 +33,7 @@ class StartingScreenComponent extends Component {
         </div>
         <ConfigForm config={this.props.config} onSubmit={this.props.onSubmit} />*/}
         <div className="loading">
-          <div className="loading__spinner animated slideInUp" style={{display: 'none'}}>
+          <div className="loading__spinner animated slideInUp">
             <div className="spinner__wrapper">
               <svg className="spinner" width="100%" height="100%" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
                 <circle className="path" fill="none" strokeWidth="2" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
@@ -47,17 +41,33 @@ class StartingScreenComponent extends Component {
             </div>
           </div>
           <div className="loading__status">
-            <div className="text">
-              <h2 className="text__headline">{this.state.headline}</h2>
-              <div className="text__details">{this.state.details}</div>
-            </div>
+            {this.state.showCarousel &&
+              <StatusCarousel>
+                {this.state.status === 0 && [
+                  <StatusPage key="1">
+                    <h2 className="text__headline">Status 0 We are running a series of tests against your site</h2>
+                    <div className="text__details">See how fast your current backend stack delivers your site to users. We will compare the results to a version of your site using Baqend Speed Kit</div>
+                  </StatusPage>
+                ]}
+                {this.state.status === 1 && [
+                  <StatusPage key="2">
+                    <h2 className="text__headline">Status 1 Malesuada Cursus Sem Ultricies</h2>
+                    <div className="text__details">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui.</div>
+                    <div className="text_details">Donec sed odio dui!</div>
+                  </StatusPage>,
+                  <StatusPage key="3">
+                    <h2 className="text__headline">Sem Aenean Fringilla Sollicitudin Tortor</h2>
+                    <div className="text__details">Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</div>
+                  </StatusPage>,
+                  <StatusPage key="4">
+                    <h2 className="text__headline">Vestibulum Tristique Vulputate Dapibus Elit</h2>
+                    <div className="text__details">Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor.</div>
+                  </StatusPage>
+                ]}
+              </StatusCarousel>
+            }
           </div>
         </div>
-        <StatusCarousel items={this.state.items} />
-        {/*<StatusCarousel>
-          <h2 className="text__headline">{this.state.headline}</h2>
-          <div className="text__details">{this.state.details}</div>
-        </StatusCarousel>*/}
       </div>
     )
   }
--- src/components/StartingScreen/StatusCarousel/StatusCarousel.js
+++ src/components/StartingScreen/StatusCarousel/StatusCarousel.js
@@ -1,58 +1,108 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import hash from 'object-hash'
 
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 import './StatusCarousel.css'
 
-class StatusCarousel extends Component {
+const flatten = arr => arr.reduce(
+  (acc, val) => acc.concat(
+    Array.isArray(val) ? flatten(val) : val
+  ), []
+)
+
+export class StatusCarousel extends Component {
   constructor(props) {
     super(props)
+    this.interval = null
+    this.pageKeys = null
+
+    this.delayFactor = 0
+    this.duration = 550
+    this.durationOffset = 150
+
     this.state = {
-      show: false,
-      items: []
+      initial: true,
+      page: 0,
+      pages: []
+    }
+  }
+
+  updatePages = (props) => {
+    const pages = flatten((Array.isArray(props.children) && props.children.filter((child) => child)) || [ props.children ])
+    const pageKeys = hash(pages.map(child => child.key))
+    if (pageKeys !== this.pageKeys) {
+      this.pageKeys = pageKeys
+
+      this.setState({
+        pages: [],
+        initial: true
+      }, () => setTimeout(() => {
+        clearInterval(this.interval)
+        this.setState({ page: 0, pages: [ ...pages ] }, () => {
+          this.setState({ initial: false })
+          if (this.state.pages.length > 1) {
+            this.interval = setInterval(() => {
+              if (this.state.page === this.state.pages.length - 1) {
+                this.setState({ page: 0,  })
+              } else {
+                this.setState({ page: this.state.page + 1 })
+              }
+            }, 6000)
+          }
+        })
+      }, this.duration))
     }
   }
 
   componentWillMount() {
-    this.setState({ items: this.props.items })
+    this.updatePages(this.props)
   }
 
   componentWillReceiveProps(nextProps) {
-    // console.log(this.props, nextProps)
-    this.setState({ items: nextProps.items })
+    this.updatePages(nextProps)
   }
-  // componentDidMount() {
-  //   this.setState({ items: this.props.items })
-  //   setInterval(() => {
-  //     if (!this.state.items.length) {
-  //       this.setState({ items: this.props.items })
-  //     } else {
-  //       this.setState({ items: [] })
-  //     }
-  //   }, 5000)
-  // }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
   render() {
     return (
       <div>
-        <div>{this.state.show ? 'true' : 'false'}</div>
         <TransitionGroup>
-          {this.state.items.map((child, i) => (
-            <CSSTransition key={i} timeout={600} classNames="text">
-              {child}
-            </CSSTransition>
-          ))}
+          {this.state.pages.map((item, j) => item.props && item.props.children.map((child, i) => {
+            if (this.state.page === j) {
+              return (
+                <CSSTransition
+                  key={i}
+                  timeout={{ enter: this.duration * 2, exit: this.duration }}
+                  classNames="text"
+                  onEnter={(node) => {
+                    const delayFactor = this.state.initial ? 0 : 1
+                    node.style.animationDuration = `0ms, ${this.duration}ms`
+                    node.style.animationDelay = `${delayFactor * this.duration}ms, ${(delayFactor * this.duration) + 50 + i * this.durationOffset}ms`
+                  }}
+                  onExit={(node) => {
+                    node.style.animationDuration = `${this.duration}ms`
+                    node.style.animationDelay = '0ms'
+                  }}
+                >
+                  {child}
+                </CSSTransition>
+              )
+            }
+          }))}
         </TransitionGroup>
       </div>
     )
   }
+
 }
 
-// ConfigForm.propTypes = {
-//   config: PropTypes.object.isRequired,
-//   onSubmit: PropTypes.func.isRequired,
-//   actions: PropTypes.object.isRequired,
-// }
-
-export default StatusCarousel
+export class StatusPage extends Component {
+  render() {
+    return this.props.children
+  }
+}
