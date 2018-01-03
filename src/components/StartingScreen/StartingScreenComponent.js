653a40932216727e03a7ddc584f8b784ca1a7613
Julian Tiemann <jutiemann@gmail.com>
2018-01-03 10:22:10 +0100

    show configform

--- src/components/StartingScreen/StartingScreenComponent.js
+++ src/components/StartingScreen/StartingScreenComponent.js
@@ -27,11 +27,11 @@ class StartingScreenComponent extends Component {
   render() {
     return (
       <div className="device device__laptop">
-        {/*<div className="text-center">
+        <div className="text-center">
           <h1>Page Speed Analyzer</h1>
           <span>Test the performance of your site!</span>
         </div>
-        <ConfigForm config={this.props.config} onSubmit={this.props.onSubmit} />*/}
+        <ConfigForm config={this.props.config} onSubmit={this.props.onSubmit} />
         <div className="loading">
           <div className="loading__spinner animated slideInUp">
             <div className="spinner__wrapper">
