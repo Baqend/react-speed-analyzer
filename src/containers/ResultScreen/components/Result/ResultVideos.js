c8d0c18dbac7ff9d20faee95712c452b139b8eb7
Julian Tiemann <jutiemann@gmail.com>
2018-02-23 10:55:53 +0100

    fix video container snipping not working

--- src/containers/ResultScreen/components/Result/ResultVideos.js
+++ src/containers/ResultScreen/components/Result/ResultVideos.js
@@ -115,7 +115,7 @@ class ResultVideos extends Component {
         <div className={`w-50 pa2 pv4-ns ph6-ns ${this.props.testOverview.mobile ? 'mobile' : ''}`}>
           <div className="video__wrapper">
             <div className="video__wrapper-inner">
-              <div className="relative" style={{ width: '100%', height: '100%'}}>
+              <div className="relative">
                 <video id="competitorVideo"
                   playsInline
                   controls={false}
@@ -149,7 +149,7 @@ class ResultVideos extends Component {
           <div className={`w-50 speedKitVideo pa2 pv4-ns ph6-ns ${this.props.testOverview.mobile ? 'mobile' : ''}`}>
             <div className="video__wrapper">
               <div className="video__wrapper-inner">
-                <div className="relative" style={{ width: '100%', height: '100%'}}>
+                <div className="relative">
                   <video id="speedKitVideo"
                     playsInline
                     controls={false}
