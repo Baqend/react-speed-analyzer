b3985c10eb34184fb74b91147e55c7588608472a
Julian Tiemann <jutiemann@gmail.com>
2018-02-28 10:51:52 +0100

    refactor runcomparison method

--- baqend/runComparison.js
+++ baqend/runComparison.js
@@ -1,6 +1,5 @@
 /* eslint-disable comma-dangle, function-paren-newline */
 /* eslint-disable no-restricted-syntax, no-param-reassign */
-
 const { isRateLimited } = require('./rateLimiter');
 const { queueTest, DEFAULT_ACTIVITY_TIMEOUT } = require('./queueTest');
 const { getTLD } = require('./getSpeedKitUrl');
@@ -8,10 +7,84 @@ const { generateUniqueId } = require('./generateUniqueId');
 const { callPageSpeed } = require('./callPageSpeed');
 const { factorize } = require('./bulkTest');
 
-/**
- * @param db The Baqend instance.
- * @param subject A test overview to finish.
- */
+function runComparison(db, params, callback = null) {
+  return new Promise((resolve, reject) => createTestOverview(db, params)
+    .then(testOverview => {
+      resolve(testOverview);
+
+      const pageSpeedInsights = getPageSpeedInfo(db, testOverview, params);
+
+      return Promise.all([
+        startCompetitorTest(db, testOverview, params, callback),
+        startSpeedKitTest(db, testOverview, params, callback)
+      ])
+      .then(([competitorTestResult, speedKitTestResult]) => {
+        testOverview.competitorTestResult = competitorTestResult;
+        testOverview.speedKitTestResult = speedKitTestResult;
+        return testOverview.ready().then(() => testOverview.save());
+      });
+    })
+  );
+}
+
+function createTestOverview(db, params) {
+  return generateUniqueId(db, 'TestOverview').then((uniqueId) => {
+    const tld = getTLD(params.url);
+    const testOverview = new db.TestOverview(Object.assign(params, {
+      id: uniqueId + tld.substring(0, tld.length - 1),
+      hasFinished: false,
+      activityTimeout: params.activityTimeout || DEFAULT_ACTIVITY_TIMEOUT
+    }));
+
+    return testOverview.save();
+  })
+}
+
+function getPageSpeedInfo(db, testOverview, { url, mobile}) {
+  return callPageSpeed(url, mobile)
+    .then(pageSpeedInsights => {
+      testOverview.partialUpdate()
+        .set('psiDomains', pageSpeedInsights.domains)
+        .set('psiRequests', pageSpeedInsights.requests)
+        .set('psiResponseSize', pageSpeedInsights.bytes)
+        .set('psiScreenshot', pageSpeedInsights.screenshot)
+        .execute();
+      return pageSpeedInsights
+    })
+    .catch(error => {
+      db.log.warn(`Could not call page speed`, { url, mobile, error: error.stack })
+    });
+}
+
+function startCompetitorTest(db, testOverview, params, callback = null) {
+  return queueTest(Object.assign(params, {
+    db: db,
+    speedKitConfig: null,
+    skipPrewarm: null,
+    isClone: false,
+    finish: (testResult) => {
+      testOverview.competitorTestResult = testResult;
+      // if (testResult.testDataMissing !== true && testResult.firstView) {
+      //   testOverview.psiDomains = testResult.firstView.domains.length;
+      //   testOverview.psiRequests = testResult.firstView.requests;
+      //   testOverview.psiResponseSize = testResult.firstView.bytes;
+      // }
+      return checkComparisonState(db, testOverview, callback);
+    }
+  }));
+}
+
+function startSpeedKitTest(db, testOverview, params, callback = null) {
+  return queueTest(Object.assign(params, {
+    db: db,
+    isClone: true,
+    finish: (testResult) => {
+      testOverview.speedKitTestResult = testResult;
+      testOverview.speedKitConfig = testResult.speedKitConfig;
+      return checkComparisonState(db, testOverview, callback);
+    }
+  }));
+}
 
 function checkComparisonState(db, testOverview, onAfterFinish) {
   if (testOverview.competitorTestResult.hasFinished && testOverview.speedKitTestResult.hasFinished) {
@@ -30,98 +103,6 @@ function calculateFactors(compResult, skResult, db) {
   }
 
   return factorize(db, compResult.firstView, skResult.firstView);
-}
-
-function runComparison(db, {
-  bulkTest,
-  location,
-  caching,
-  url,
-  activityTimeout,
-  speedKitConfig,
-  isSpeedKitComparison,
-  speedKitVersion,
-  whitelist,
-  mobile,
-  priority,
-  skipPrewarm
-}, callback = null) {
-  return new Promise((resolve, reject) => {
-    const testOverview = new db.TestOverview();
-    return generateUniqueId(db, 'TestOverview').then((uniqueId) => {
-      const tld = getTLD(url);
-      testOverview.id = uniqueId + tld.substring(0, tld.length - 1);
-      testOverview.location = location;
-      testOverview.url = url;
-      testOverview.whitelist = whitelist;
-      testOverview.caching = caching;
-      testOverview.mobile = mobile;
-      testOverview.hasFinished = false;
-      testOverview.speedKitConfig = speedKitConfig;
-      testOverview.isSpeedKitComparison = isSpeedKitComparison;
-      testOverview.speedKitVersion = speedKitVersion;
-      testOverview.activityTimeout = activityTimeout || DEFAULT_ACTIVITY_TIMEOUT;
-
-      return testOverview.save();
-    }).then((testOverview) => {
-      resolve(testOverview);
-
-      callPageSpeed(url, mobile).then(pageSpeedInsightsResult => testOverview.partialUpdate()
-        .set('psiDomains', pageSpeedInsightsResult.domains)
-        .set('psiRequests', pageSpeedInsightsResult.requests)
-        .set('psiResponseSize', pageSpeedInsightsResult.bytes)
-        .set('psiScreenshot', pageSpeedInsightsResult.screenshot)
-        .execute()
-      ).catch(error => db.log.warn(`Could not call page speed`, { url, mobile, error: error.stack }));
-
-      const competitorTestRun = queueTest({
-        db,
-        location,
-        caching,
-        url,
-        activityTimeout,
-        priority,
-        isSpeedKitComparison,
-        mobile,
-        isClone: false,
-        finish: (testResult) => {
-          testOverview.competitorTestResult = testResult;
-          // if (testResult.testDataMissing !== true && testResult.firstView) {
-          //   testOverview.psiDomains = testResult.firstView.domains.length;
-          //   testOverview.psiRequests = testResult.firstView.requests;
-          //   testOverview.psiResponseSize = testResult.firstView.bytes;
-          // }
-          return checkComparisonState(db, testOverview, callback);
-        }
-      });
-
-      const speedKitTestRun = queueTest({
-        db,
-        location,
-        caching,
-        url,
-        activityTimeout,
-        priority,
-        isSpeedKitComparison,
-        mobile,
-        speedKitConfig,
-        skipPrewarm,
-        isClone: true,
-        finish: (testResult) => {
-          testOverview.speedKitTestResult = testResult;
-          testOverview.speedKitConfig = testResult.speedKitConfig;
-          return checkComparisonState(db, testOverview, callback);
-        }
-      });
-
-      return Promise.all([competitorTestRun, speedKitTestRun])
-        .then(([competitorTestResult, speedKitTestResult]) => {
-          testOverview.competitorTestResult = competitorTestResult;
-          testOverview.speedKitTestResult = speedKitTestResult;
-          return testOverview.ready().then(() => testOverview.save());
-        });
-    });
-  });
 }
 
 exports.post = (db, req, res) => {
