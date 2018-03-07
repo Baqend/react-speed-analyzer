ba4f82d27c0ca88cb0af31ca5e426b70b414231f
Hannes Kuhlmann <hk@baqend.com>
2018-03-07 19:01:45 +0100

    use credentials file

--- baqend/deleteCronjob.js
+++ baqend/deleteCronjob.js
@@ -1,10 +1,12 @@
+const credentials = require('./deleteCronjobCredentials');
+
 exports.run = function(db) {
   return db.User.logout().then(() => {
-    return db.User.login('adminUser', 'GdrsF+DZ]2,+~4.R')
+    return db.User.login(credentials.user, credentials.password)
   }).then(() => {
     let folder = db.File('/file/baqend_assets/');
     return folder.delete()
   }).then(() => {
     return db.send(new db.message.DeleteBloomFilter());
   });
-}
+};
