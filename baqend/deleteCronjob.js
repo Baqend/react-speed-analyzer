e0eb4be532e6bc872d6ae70cf0e4fecd930eca66
Hannes Kuhlmann <hk@baqend.com>
2018-03-07 19:07:23 +0100

    truncate speed kit table in delete cronjob

--- baqend/deleteCronjob.js
+++ baqend/deleteCronjob.js
@@ -2,10 +2,12 @@ const credentials = require('./deleteCronjobCredentials');
 
 exports.run = function(db) {
   return db.User.logout().then(() => {
-    return db.User.login(credentials.user, credentials.password)
+    return db.User.login(credentials.user, credentials.password);
+  }).then(() => {
+    return db.send(new db.message.TruncateBucket('speedKit.Asset'));
   }).then(() => {
     let folder = db.File('/file/baqend_assets/');
-    return folder.delete()
+    return folder.delete();
   }).then(() => {
     return db.send(new db.message.DeleteBloomFilter());
   });
