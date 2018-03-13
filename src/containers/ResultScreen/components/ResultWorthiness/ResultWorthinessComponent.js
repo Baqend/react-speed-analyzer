0f56e2c33cda25ed45d4e5cdf55692063d4d26c2
Julian Tiemann <jutiemann@gmail.com>
2018-03-13 11:55:36 +0100

    text changes

--- src/containers/ResultScreen/components/ResultWorthiness/ResultWorthinessComponent.js
+++ src/containers/ResultScreen/components/ResultWorthiness/ResultWorthinessComponent.js
@@ -24,13 +24,12 @@ class ResultWorthinessComponent extends Component {
       <div>
         <div className="flex">
           <div className="w-100 text-center mb4 mt4 animated slideInUp">
-            <h1 className="dn db-ns" style={{ maxWidth: 768, margin: '0 auto' }}>
-              Speed Kit made your site <span style={{ color: '#F27354' }}>{absolute}</span> faster by serving {servedRate}% of all requests.
-            </h1>
-            <h3 className="dn-ns" style={{ maxWidth: 768, margin: '0 auto' }}>
-              Speed Kit made your site <span style={{ color: '#F27354' }}>{absolute}</span> faster by serving {servedRate}% of all requests.
+            <h2 className="dn db-ns mt0" style={{ maxWidth: 768, marginLeft: 'auto', marginRight: 'auto' }}>
+              How much is the <span style={{ color: '#F27354' }}>{absolute}</span> performance boost worth?
+            </h2>
+            <h3 className="dn-ns mt0" style={{ maxWidth: 768, marginLeft: 'auto', marginRight: 'auto' }}>
+              How much is the <span style={{ color: '#F27354' }}>{absolute}</span> performance boost worth?
             </h3>
-            <br />
             <h4 className="faded" style={{ maxWidth: 530, margin: '0 auto' }}>
               Here is the impact Google and Amazon research predicts for this uplift.
             </h4>
