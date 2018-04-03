1735b118af85481a8024210ae71d7d0178aa3afb
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2018-04-03 16:33:13 +0200

    Refactor BulkComparisonWorker.next

--- baqend/_bulkComparisonWorker.js
+++ baqend/_bulkComparisonWorker.js
@@ -10,26 +10,28 @@ class BulkComparisonWorker {
   next(bulkComparisonId) {
     this.db.log.info("BulkComparisonWorker next", bulkComparisonId)
     this.db.BulkComparison.load(bulkComparisonId, {depth: 1})
-      .then(bulkComparison => bulkComparison.ready().then(() => {
+      .then(bulkComparison => bulkComparison.ready())
+      .then(bulkComparison => {
         const { multiComparisons, createdBy } = bulkComparison
         const currentMultiComparison = multiComparisons[multiComparisons.length - 1]
-
-        if (multiComparisons.length < 1 || currentMultiComparison.hasFinished) {
-          const nextMultipleComparison = this.getNextMultipleComparison(bulkComparison)
-          if (!nextMultipleComparison) {
-            bulkComparison.hasFinished = true
-            bulkComparison.ready().then(() => bulkComparison.save())
-          } else {
-            const multiComparisonRequest = new MultiComparisonRequest(this.db, createdBy, nextMultipleComparison)
-            multiComparisonRequest.create().then(multiComparison => {
-              bulkComparison.multiComparisons.push(multiComparison)
-              bulkComparison.ready().then(() => bulkComparison.save())
-
-              this.multiComparisonWorker.next(multiComparison.id)
-            });
-          }
+        if (multiComparisons.length > 0 && !currentMultiComparison.hasFinished) {
+          return
         }
-    }))
+
+        const nextMultipleComparison = this.getNextMultipleComparison(bulkComparison)
+        if (!nextMultipleComparison) {
+          return bulkComparison.optimisticSave((it) => {
+            it.hasFinished = true
+          })
+        }
+        const multiComparisonRequest = new MultiComparisonRequest(this.db, createdBy, nextMultipleComparison)
+        multiComparisonRequest.create().then(multiComparison => {
+          bulkComparison.multiComparisons.push(multiComparison)
+          bulkComparison.ready().then(() => bulkComparison.save())
+
+          this.multiComparisonWorker.next(multiComparison.id)
+        })
+      })
     .catch(error => this.db.log.warn(`Error while next iteration`, {id: bulkComparisonId, error: error.stack}))
   }
 
