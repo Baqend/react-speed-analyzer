6851c5ea114a95694a545f656a416205876d3f9d
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2018-04-03 18:53:00 +0200

    Refactor download.ts

--- baqend/ts/download.ts
+++ baqend/ts/download.ts
@@ -1,4 +1,6 @@
-const http = require('http');
+import http from 'http'
+import { baqend, binding } from 'baqend'
+
 /**
  * Fetches a URL and saves it to a Baqend file.
  *
@@ -12,40 +14,38 @@ const http = require('http');
  * @param maxRetries number of retries if the response has a 4xx or 5xx status code
  * @returns {Promise} a Promise that resolves to the uploaded file
  */
-function toFile(db, url, target, maxRetries = 10) {
-    return new Promise((resolve, reject) => {
-        http.get(url, (res) => {
-            const file = new db.File({path: target});
-            const size = res.headers['content-length'];
-            const mimeType = res.headers['content-type'];
+export function toFile(db: baqend, url: string, target: string, maxRetries: number = 10): Promise<binding.File> {
+  return new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      const file = new db.File({ path: target })
+      const size = res.headers['content-length']
+      const mimeType = res.headers['content-type']
 
-            //Retry on error
-            if (res.statusCode >= 400 && res.statusCode < 600) {
-                if(maxRetries <= 0) {
-                    reject(new Error("Maximum number of retries reached without success"));
-                } else {
-                    setTimeout(() => resolve(toFile(db, url, target, maxRetries - 1)), 500);
-                }
-                return;
-            }
+      // Retry on error
+      if (res.statusCode >= 400 && res.statusCode < 600) {
+        if (maxRetries <= 0) {
+          reject(new Error('Maximum number of retries reached without success'))
+        } else {
+          setTimeout(() => resolve(toFile(db, url, target, maxRetries - 1)), 500)
+        }
+        return
+      }
 
-            //Full response
-            if (size) {
-                resolve(file.upload({mimeType, size, type: 'stream', data: res}));
-                return;
-            }
+      // Full response
+      if (size) {
+        resolve(file.upload({ mimeType, size, type: 'stream', data: res }))
+        return
+      }
 
-            //Or chunked encoding
-            const chunks = [];
-            res.on('data', chunks.push.bind(chunks));
-            res.on('end', () => {
-                const buf = Buffer.concat(chunks);
-                resolve(file.upload({mimeType, size: buf.length, type: 'buffer', data: buf, force: true}));
-            });
-        }).on('error', (e) => {
-            reject(e);
-        });
-    });
+      // Or chunked encoding
+      const chunks = []
+      res.on('data', chunks.push.bind(chunks))
+      res.on('end', () => {
+        const buf = Buffer.concat(chunks)
+        resolve(file.upload({ mimeType, size: buf.length, type: 'buffer', data: buf, force: true }))
+      })
+    }).on('error', (e) => {
+      reject(e)
+    })
+  })
 }
-
-exports.toFile = toFile;
