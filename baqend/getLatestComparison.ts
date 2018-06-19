adb4ffc3c06bb5d80adc9bc54e64d68e107aecb2
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2018-06-19 14:50:39 +0200

    Refacor getLatestComparison

--- baqend/getLatestComparison.ts
+++ baqend/getLatestComparison.ts
@@ -6,16 +6,17 @@ import { Status } from './_Status'
  * GET: Get latest successful comparison of a given domain.
  */
 export async function get(db: baqend, request: Request, response: Response) {
-  const url = request.query.url
+  const { query: { url } } = request
   if (!url) {
-    throw new Abort('You have to provide a url.')
+    throw new Abort('You have to provide a "url" query parameter.')
   }
 
-  const latestComparison: model.TestOverview = await db.TestOverview.find()
+  const latestComparison: model.TestOverview | null = await db.TestOverview.find()
     .eq('url', url)
     .eq('status', Status.SUCCESS)
     .descending('updatedAt')
     .singleResult()
+  const { factors = null } = latestComparison
 
-  response.send({ factors: latestComparison ? latestComparison.factors : null })
+  response.send({ factors })
 }
