fa5185084a79069ff23202476e00f46854807055
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2019-01-04 17:50:16 +0100

    Let startComparison retry Puppeteer calls

--- baqend/startComparison.ts
+++ baqend/startComparison.ts
@@ -5,42 +5,52 @@ import { TestParams } from './_TestParams'
 
 const MAX_PUPPETEER_RETRIES = 2;
 
-async function updateWithPuppeteer(
-  db: baqend,
-  params: TestParams,
-  comparison: model.TestOverview,
-  retries: number = 0,
-): Promise<model.TestOverview> {
-  const { comparisonWorker, comparisonFactory, puppeteer } = bootstrap(db)
-
-  try {
-    const puppeteerInfo = await puppeteer.analyze(params.url, params.mobile, params.location, params.preload)
-    // Retry Puppeteer analysis if Speed Kit is excepted to be installed but no installation was found.
-    if (params.speedKitExpected && !puppeteerInfo.speedKit && retries <= MAX_PUPPETEER_RETRIES) {
-      return updateWithPuppeteer(db, params, comparison, retries + 1)
-    }
-
-    const updatedComparison = await comparisonFactory.updateComparison(comparison, puppeteerInfo, params)
-    comparisonWorker.next(updatedComparison).catch((err) => db.log.error(err.message, err))
-
-    return updatedComparison
-  } catch ({ message, status = 500 }) {
-    await comparisonFactory.updateComparisonWithError(comparison, message, status)
-    return comparison
-  }
-}
-
 /**
  * Baqend code API call.
  */
 export async function post(db: baqend, req: Request, res: Response) {
-  const { comparisonFactory } = bootstrap(db)
+  const { comparisonWorker, comparisonFactory, puppeteer } = bootstrap(db)
+
+  const callPuppeteer = async (params: TestParams) => {
+    const puppeteerInfo = await puppeteer.analyze(params.url, params.mobile, params.location, params.preload)
+    // Retry Puppeteer analysis if Speed Kit is excepted to be installed but no installation was found.
+    if (params.speedKitExpected && !puppeteerInfo.speedKit) {
+      throw new Error('Expected result to be Speed Kit, but was not.')
+    }
+
+    return puppeteerInfo
+  }
+
+  const callPuppeteerWithRetries = async (params: TestParams, retries = 0): Promise<model.Puppeteer> => {
+    try {
+      return await callPuppeteer(params)
+    } catch (err) {
+      if (retries <= MAX_PUPPETEER_RETRIES) {
+        return callPuppeteerWithRetries(params, retries + 1);
+      }
+
+      throw err
+    }
+  }
+
+  const updateWithPuppeteer = async (params: TestParams, comparison: model.TestOverview) => {
+    try {
+      const puppeteerInfo = await callPuppeteerWithRetries(params)
+      const updatedComparison = await comparisonFactory.updateComparison(comparison, puppeteerInfo, params)
+      comparisonWorker.next(updatedComparison).catch((err) => db.log.error(err.message, err))
+
+      return updatedComparison
+    } catch ({ message, status = 500 }) {
+      await comparisonFactory.updateComparisonWithError(comparison, message, status)
+      return comparison
+    }
+  }
 
   const { withPuppeteer = true, ...params } = req.body as { withPuppeteer?: boolean } & TestParams
   const comparison = await comparisonFactory.createComparison(params.url)
 
   if (withPuppeteer) {
-    const updatedComparison = await updateWithPuppeteer(db, params, comparison)
+    const updatedComparison = await updateWithPuppeteer(params, comparison)
 
     res.status(200)
     res.send(updatedComparison)
@@ -48,7 +58,7 @@ export async function post(db: baqend, req: Request, res: Response) {
   }
 
   // async call to update the testOverview with the puppeteer data
-  updateWithPuppeteer(db, params, comparison)
+  updateWithPuppeteer(params, comparison)
 
   res.status(200)
   res.send(comparison)
