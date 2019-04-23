663516c38abb9a8554201b34711d8f150a274574
Kevin Twesten <kevin.twesten@baqend.com>
2019-04-23 14:36:22 +0200

    fix: Fix postcss config for embedded version

--- config/postcss-scoped/postcss.config.js
+++ config/postcss-scoped/postcss.config.js
@@ -1,17 +1,24 @@
 module.exports = {
-  plugins: {
-    'postcss-import': {
-      path: ['./src', '../src']
-    },
-    'postcss-nested': {},
-    'postcss-calc': {},
-    'postcss-cssnext': {
+  plugins: [
+    require('postcss-import')({
+      path: ['./src', '../src'],
+      resolve: function resolve(id, basedir, importOptions) {
+        if (id.startsWith('~')) {
+          return require.resolve(id.substring(1))
+        }
+
+        return id
+      }
+    }),
+    require('postcss-nested')(),
+    require('postcss-calc')(),
+    require('postcss-cssnext')({
       browsers: ['last 2 versions', '> 5%'],
-    },
-    'postcss-flexbugs-fixes': {},
-    'postcss-wrap': {
+    }),
+    require('postcss-flexbugs-fixes')(),
+    require('postcss-wrap')({
       selector: '#speed-kit-analyzer',
       skip: ['speed-kit-analyzer']
-    },
-  },
+    }),
+  ],
 }
