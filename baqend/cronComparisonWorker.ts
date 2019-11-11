babe7785893acadc40e708fedf748065625ab558
Kevin Twesten <kevin.twesten@baqend.com>
2019-11-11 12:17:22 +0100

    refactor: Increase interval to two minutes

--- baqend/cronComparisonWorker.ts
+++ baqend/cronComparisonWorker.ts
@@ -2,6 +2,7 @@ import { baqend } from 'baqend'
 import { bootstrap } from './_compositionRoot'
 
 const ONE_MINUTE = 1000 * 60
+const TWO_MINUTES = ONE_MINUTE * 2
 const TWO_DAYS = ONE_MINUTE * 60 * 24 * 2
 
 /**
@@ -14,7 +15,7 @@ export async function run(db: baqend) {
   // Ascending by "updatedAt" to start at the head of the queue.
   const comparisons = await db.TestOverview.find()
     .equal('hasFinished', false)
-    .lessThanOrEqualTo('updatedAt', new Date(now - ONE_MINUTE))
+    .lessThanOrEqualTo('updatedAt', new Date(now - TWO_MINUTES))
     .greaterThanOrEqualTo('updatedAt', new Date(now - TWO_DAYS))
     .ascending('updatedAt')
     .resultList({ depth: 1 })
