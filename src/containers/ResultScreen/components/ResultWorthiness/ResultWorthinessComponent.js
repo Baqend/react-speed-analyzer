1ba85f9819e50e6a04ae414ceb48d0fe181e8332
Kevin Twesten <kt@baqend.com>
2020-06-17 10:59:43 +0200

    refactor: Minor styling improvements

--- src/containers/ResultScreen/components/ResultWorthiness/ResultWorthinessComponent.js
+++ src/containers/ResultScreen/components/ResultWorthiness/ResultWorthinessComponent.js
@@ -30,31 +30,31 @@ class ResultWorthinessComponent extends Component {
         <h2 className="ma0">What can you expect from this <span className="purple">{savedMilliseconds} ms</span> improvement?</h2>
         <h3>Here is a range of potential outcomes from well-known studies.</h3>
         <div className="flex flex-wrap text-center" style={{margin: '0 -10px'}}>
-          <div className="ma1 box-wrapper">
-            <div className="worthiness-header" style={{ margin: '10px' }}>
+          <div className="box-wrapper" style={{margin: '10px'}}>
+            <div className="worthiness-header">
               <span className="purple">+{(savedMilliseconds/100).toFixed(2)}%</span> Revenue
             </div>
             <div className="text-light-grey">100 ms ≙ 1% Revenue</div>
             <a className="btn btn-blue btn-ghost mt4" href={this.getStudyLink('amazon')} target="_blank">Amazon Study</a>
           </div>
-          <div className="ma1 box-wrapper">
-            <div className="worthiness-header" style={{ margin: '10px' }}>
+          <div className="box-wrapper" style={{margin: '10px'}}>
+            <div className="worthiness-header">
               <span className="purple">+{(savedMilliseconds/100*0.8).toFixed(2)}%</span> Conversions
             </div>
             <div className="text-light-grey">100 ms ≙ 0.8% Conversions</div>
             <a className="btn btn-blue btn-ghost mt4" href={this.getStudyLink('eggplant')} target="_blank">Eggplant Study</a>
           </div>
         </div>
-        <div className="flex flex-wrap text-center" style={{ margin: '0 -10px' }}>
-          <div className="ma1 box-wrapper">
-            <div className="worthiness-header" style={{ margin: '10px' }}>
+        <div className="flex flex-wrap text-center" style={{margin: '0 -10px'}}>
+          <div className="box-wrapper" style={{margin: '10px'}}>
+            <div className="worthiness-header">
               <span className="purple">+{(savedMilliseconds/100*1.6).toFixed(2)}%</span> Traffic
             </div>
             <div className="text-light-grey">100 ms ≙ 1.6% Traffic</div>
             <a className="btn btn-blue btn-ghost mt4" href={this.getStudyLink('gq')} target="_blank">GQ Study</a>
           </div>
-          <div className="ma1 box-wrapper">
-            <div className="worthiness-header" style={{ margin: '10px' }}>
+          <div className="box-wrapper" style={{margin: '10px'}}>
+            <div className="worthiness-header">
               <span className="purple">+{(savedMilliseconds/100*0.4).toFixed(2)}%</span> SEO-Traffic
             </div>
             <div className="text-light-grey">100 ms ≙ 0.4% SEO-Traffic</div>
@@ -62,15 +62,15 @@ class ResultWorthinessComponent extends Component {
           </div>
         </div>
         <div className="flex flex-wrap text-center" style={{margin: '0 -10px'}}>
-          <div className="ma1 box-wrapper">
-            <div className="worthiness-header" style={{ margin: '10px' }}>
+          <div className="box-wrapper" style={{margin: '10px'}}>
+            <div className="worthiness-header">
               <span className="purple">+{(savedMilliseconds/100*0.6).toFixed(2)}%</span> Session Length
             </div>
             <div className="text-light-grey">100 ms ≙ 0.6% Session Length</div>
             <a className="btn btn-blue btn-ghost mt4" href={this.getStudyLink('otto')} target="_blank">OTTO Study</a>
           </div>
-          <div className="ma1 box-wrapper">
-            <div className="worthiness-header" style={{ margin: '10px' }}>
+          <div className="box-wrapper" style={{margin: '10px'}}>
+            <div className="worthiness-header">
               <span className="purple">-{(savedMilliseconds/1000*18.4).toFixed(2)}%</span> Bounce Rate
             </div>
             <div className="text-light-grey">1 s ≙ -18.4% Bounce Rate</div>
