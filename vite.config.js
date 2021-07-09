2632b6308b80520efdd9bf3d90577d9d856e81f1
Hannes <hk@baqend.com>
2021-07-09 15:44:49 +0200

    chore: remove unused import from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, normalizePath } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
