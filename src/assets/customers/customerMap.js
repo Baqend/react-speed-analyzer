2a113b98bdad7d12de141e7cf6219dbd4a0120d1
JLCD <jean-luc@jlcd.de>
2023-01-30 10:24:56 +0100

    fix: customers

--- src/assets/customers/customerMap.js
+++ src/assets/customers/customerMap.js
@@ -8,13 +8,11 @@ import mydaysLogo from "./mydays-black.svg";
 import redBullLogo from "./redbull-black.svg";
 import hsvLogo from "./hsv-black.svg";
 import titusLogo from "./titus-black.svg";
-import justSpicesLogo from "./justspices-black.svg";
 import defshopLogo from "./DefShop_Logo.svg";
 import edwinLogo from "./edwin-black.png";
 import bugabooLogo from "./bugaboo-black.png";
 import brogleLogo from "./brogle-black.png";
 import soleboxLogo from "./solebox-black.svg";
-import weinfuerstLogo from "./weinfuerst-black.svg";
 import besamexLogo from "./besamex-black.png";
 import melittaLogo from "./melitta-black.svg";
 import pleskLogo from "./plesk-black.svg";
@@ -22,7 +20,6 @@ import awnLogo from "./awn-black.png";
 import goertzLogo from "./goertz-black.svg";
 import bmw from "./bmw-black.svg";
 import newportLogo from "./newport-black.svg";
-import baurLogo from "./baur-black.svg";
 import elevenTeamsportsLogo from "./11teamsports-black.svg";
 import interfloraLogo from "./interflora-black.svg";
 import globusLogo from "./globus-ch-black.svg";
@@ -41,12 +38,10 @@ export const CUSTOMER_MAP = {
   Defshop: defshopLogo,
   HSV: hsvLogo,
   Titus: titusLogo,
-  JustSpices: justSpicesLogo,
   Edwin: edwinLogo,
   Bugaboo: bugabooLogo,
   Brogle: brogleLogo,
   Solebox: soleboxLogo,
-  Weinfürst: weinfuerstLogo,
   Besamex: besamexLogo,
   Melitta: melittaLogo,
   Plesk: pleskLogo,
@@ -55,7 +50,6 @@ export const CUSTOMER_MAP = {
   Goertz: goertzLogo,
   "Pierre-cardin": piereCardinLogo,
   newport: newportLogo,
-  Baur: baurLogo,
   one: oneLogo,
   giordano: giordanoLogo,
   "eleven-teamsports": elevenTeamsportsLogo,
