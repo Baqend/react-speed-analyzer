2db088886ce278aec882733ada91fe7db8450021
Jean-Luc Christoph Dittler <jean-luc.dittler@baqend.com>
2023-04-27 10:43:05 +0200

    feat(startscreen) added config control via url params

--- src/actions/config.js
+++ src/actions/config.js
@@ -8,61 +8,65 @@ import {
   SWITCH_MOBILE,
   SWITCH_CACHING,
   UPDATE_CONFIG,
-} from './types'
+} from "./types";
 
 export function resetConfig() {
   return {
-    type: RESET_CONFIG
-  }
+    type: RESET_CONFIG,
+  };
 }
 
 export function handleUrlInput(cookie) {
   return {
     type: CHANGE_URL,
     payload: cookie,
-  }
+  };
 }
 
 export function handleCookieInput(url) {
   return {
     type: CHANGE_COOKIE,
     payload: url,
-  }
+  };
 }
 
 export function handleLocationChange(location) {
+  location = location.toLowerCase();
   return {
     type: CHANGE_LOCATION,
-    payload: location === 'US' ? 'us-east-1-docker:Chrome.FIOSNoLatency' : 'eu-central-1-docker:Chrome.FIOSNoLatency',
-  }
+    payload:
+      location === "us"
+        ? "us-east-1-docker:Chrome.FIOSNoLatency"
+        : "eu-central-1-docker:Chrome.FIOSNoLatency",
+  };
 }
 
 export function handleTimeoutChange(timeout) {
   return {
     type: CHANGE_TIMEOUT,
     payload: timeout,
-  }
+  };
 }
 
 export function handleSpeedKitConfigChange(config) {
   return {
     type: CHANGE_SPEED_KIT_CONFIG,
     payload: config,
-  }
+  };
 }
 
 export function handleMobileSwitch(mobile) {
   return {
     type: SWITCH_MOBILE,
     payload: !mobile,
-  }
+  };
 }
 
 export function handleCachingSwitch(caching) {
   return {
     type: SWITCH_CACHING,
     payload: !caching,
-  }
+  };
 }
 
 export const updateConfigByTestOverview = (testOverview) => ({
@@ -74,5 +78,5 @@ export const updateConfigByTestOverview = (testOverview) => ({
     mobile: testOverview.mobile,
     activityTimeout: testOverview.activityTimeout,
     speedKitConfig: testOverview.speedKitConfig,
-  }
-})
+  },
+});
