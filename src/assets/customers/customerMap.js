a54229db7e50994dd2e6ba977fe11de51ee9a24e
JLCD <jean-luc@jlcd.de>
2023-12-14 13:16:35 +0100

    feat(customers) updated logos

--- src/assets/customers/customerMap.js
+++ src/assets/customers/customerMap.js
@@ -1,11 +1,8 @@
 import lebara from "./lebara-black.png";
 import galeriaLogo from "./galeria-black.svg";
-import piereCardinLogo from "./pierre-cardin-black.png";
 import decathlonLogo from "./decathlon-black.svg";
 import carharttLogo from "./carhartt-black.svg";
 import snipesLogo from "./snipes-black.svg";
-import mydaysLogo from "./mydays-black.svg";
-import redBullLogo from "./redbull-black.svg";
 import hsvLogo from "./hsv-black.svg";
 import titusLogo from "./titus-black.svg";
 import defshopLogo from "./DefShop_Logo.svg";
@@ -13,18 +10,13 @@ import edwinLogo from "./edwin-black.png";
 import bugabooLogo from "./bugaboo-black.png";
 import brogleLogo from "./brogle-black.png";
 import soleboxLogo from "./solebox-black.svg";
-import besamexLogo from "./besamex-black.png";
 import melittaLogo from "./melitta-black.svg";
 import pleskLogo from "./plesk-black.svg";
-import awnLogo from "./awn-black.png";
-import goertzLogo from "./goertz-black.svg";
 import bmw from "./bmw-black.svg";
 import newportLogo from "./newport-black.svg";
 import elevenTeamsportsLogo from "./11teamsports-black.svg";
 import interfloraLogo from "./interflora-black.svg";
-import globusLogo from "./globus-ch-black.svg";
 import virginmediaLogo from "./virginmedia-black.svg";
-import giordanoLogo from "./giordano-black.png";
 import oneLogo from "./onede-black.svg";
 
 export const CUSTOMER_MAP = {
@@ -33,8 +25,6 @@ export const CUSTOMER_MAP = {
   Decathlon: decathlonLogo,
   Carhartt: carharttLogo,
   Snipes: snipesLogo,
-  Mydays: mydaysLogo,
-  RedBull: redBullLogo,
   Defshop: defshopLogo,
   HSV: hsvLogo,
   Titus: titusLogo,
@@ -42,18 +32,12 @@ export const CUSTOMER_MAP = {
   Bugaboo: bugabooLogo,
   Brogle: brogleLogo,
   Solebox: soleboxLogo,
-  Besamex: besamexLogo,
   Melitta: melittaLogo,
   Plesk: pleskLogo,
-  Awn: awnLogo,
   Lebara: lebara,
-  Goertz: goertzLogo,
-  "Pierre-cardin": piereCardinLogo,
   newport: newportLogo,
   one: oneLogo,
-  giordano: giordanoLogo,
   "eleven-teamsports": elevenTeamsportsLogo,
   interflora: interfloraLogo,
-  globus: globusLogo,
   virginmedia: virginmediaLogo,
 };
