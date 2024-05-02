67f6fd88277e5db1f8eb8bb5ad49328904aae30f
Julian Schenkemeyer <julian.schenkemeyer@baqend.com>
2024-05-02 15:10:48 +0200

    fix(device bezel): device bezel changes according to the selected device

--- src/components/ConfigForm/ConfigForm.js
+++ src/components/ConfigForm/ConfigForm.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-import ConfigFormComponent from './ConfigFormComponent'
+import ConfigFormComponent from "./ConfigFormComponent";
 
 import {
   handleUrlInput,
@@ -12,37 +12,38 @@ import {
   handleTimeoutChange,
   handleSpeedKitConfigChange,
   handleMobileSwitch,
-  handleCachingSwitch
-} from '../../actions/config'
+  handleCachingSwitch,
+} from "../../actions/config";
 
 class ConfigForm extends Component {
   onUrlChange = (data) => {
-    this.props.actions.handleUrlInput(data)
-  }
+    this.props.actions.handleUrlInput(data);
+  };
 
   onCookieChange = (data) => {
-    this.props.actions.handleCookieInput(data)
-  }
+    this.props.actions.handleCookieInput(data);
+  };
 
   onLocationChange = (data) => {
-    this.props.actions.handleLocationChange(data)
-  }
+    this.props.actions.handleLocationChange(data);
+  };
 
   onTimeoutChange = (data) => {
-    this.props.actions.handleTimeoutChange(data)
-  }
+    this.props.actions.handleTimeoutChange(data);
+  };
 
   onSpeedKitConfigChange = (data) => {
-    this.props.actions.handleSpeedKitConfigChange(data)
-  }
+    this.props.actions.handleSpeedKitConfigChange(data);
+  };
 
-  onMobileSwitch = () => {
-    this.props.actions.handleMobileSwitch(this.props.config.mobile)
-  }
+  onMobileSwitch = (data) => {
+    console.log({ data, mobile: this.props.config.mobile });
+    this.props.actions.handleMobileSwitch(data);
+  };
 
   onCachingSwitch = () => {
-    this.props.actions.handleCachingSwitch(this.props.config.caching)
-  }
+    this.props.actions.handleCachingSwitch(this.props.config.caching);
+  };
 
   render() {
     return (
@@ -63,7 +64,7 @@ class ConfigForm extends Component {
         onCachingSwitch={this.onCachingSwitch}
         onToggleAdvancedConfig={this.props.onToggleAdvancedConfig}
       />
-    )
+    );
   }
 }
 
@@ -71,7 +72,7 @@ ConfigForm.defaultProps = {
   showConfig: true,
   showConfigToggle: true,
   showAdvancedConfig: false,
-}
+};
 
 ConfigForm.propTypes = {
   showConfig: PropTypes.bool,
@@ -82,27 +83,30 @@ ConfigForm.propTypes = {
   config: PropTypes.object.isRequired,
   actions: PropTypes.object.isRequired,
   whiteListCandidates: PropTypes.array,
-}
+};
 
 function mapStateToProps(state) {
   return {
     isInitiated: state.result.isInitiated,
-    whiteListCandidates: state.result.whiteListCandidates
-  }
+    whiteListCandidates: state.result.whiteListCandidates,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators({
-      handleUrlInput,
-      handleCookieInput,
-      handleLocationChange,
-      handleTimeoutChange,
-      handleSpeedKitConfigChange,
-      handleMobileSwitch,
-      handleCachingSwitch,
-    }, dispatch),
-  }
+    actions: bindActionCreators(
+      {
+        handleUrlInput,
+        handleCookieInput,
+        handleLocationChange,
+        handleTimeoutChange,
+        handleSpeedKitConfigChange,
+        handleMobileSwitch,
+        handleCachingSwitch,
+      },
+      dispatch
+    ),
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ConfigForm)
+export default connect(mapStateToProps, mapDispatchToProps)(ConfigForm);
