dd485308107792470a309ed512a72030f8124560
Julian Schenkemeyer <julian.schenkemeyer@baqend.com>
2024-05-03 09:05:40 +0200

    refactor(ResultHeader): show advanced options toggle

--- src/containers/ResultScreen/components/Result/ResultHeader.js
+++ src/containers/ResultScreen/components/Result/ResultHeader.js
@@ -1,53 +1,50 @@
-import React, { Component } from 'react'
-import { isSpeedKitInstalledCorrectly } from '../../../../helper/resultHelper'
-import ResultVideos from './ResultVideos'
-import './ResultHeader.css'
-import ResultMainMetric from './ResultMainMetric'
-import Header from 'components/Header/Header'
-import ConfigForm from 'components/ConfigForm/ConfigForm'
+import React, { Component } from "react";
+import { isSpeedKitInstalledCorrectly } from "../../../../helper/resultHelper";
+import ResultVideos from "./ResultVideos";
+import "./ResultHeader.css";
+import ResultMainMetric from "./ResultMainMetric";
+import Header from "components/Header/Header";
+import ConfigForm from "components/ConfigForm/ConfigForm";
 
 class ResultHeader extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       showConfig: props.showConfig,
       showAdvancedConfig: props.showAdvancedConfig,
-    }
+    };
   }
 
   renderMainMetric() {
-    return (
-      <ResultMainMetric { ...this.props } />
-    )
+    return <ResultMainMetric {...this.props} />;
   }
 
   renderVideos() {
-    return (
-      <ResultVideos { ...this.props } />
-    )
+    return <ResultVideos {...this.props} />;
   }
 
   renderConfigForm() {
-    const { isPlesk, embedded } = this.props
+    const { isPlesk, embedded } = this.props;
     return (
-      <div className={`${embedded && !isPlesk ? 'pt7' : 'pt5'}`}>
+      <div className={`${embedded && !isPlesk ? "pt7" : "pt5"}`}>
         <ConfigForm
           config={this.props.config}
-          showConfig={this.state.showConfig}
+          showConfig={true}
           showAdvancedConfig={this.state.showAdvancedConfig}
           showConfigToggle={true}
           onToggleAdvancedConfig={this.props.onToggleAdvancedConfig}
           onSubmit={this.props.onSubmit}
         />
       </div>
-    )
+    );
   }
 
   render() {
-    const { isPlesk, embedded } = this.props
-    const { testOverview } = this.props.result
-    const isWordPress = testOverview.type === 'wordpress'
-    const { isSpeedKitComparison, speedKitVersion, configAnalysis } = testOverview
+    const { isPlesk, embedded } = this.props;
+    const { testOverview } = this.props.result;
+    const isWordPress = testOverview.type === "wordpress";
+    const { isSpeedKitComparison, speedKitVersion, configAnalysis } =
+      testOverview;
 
     return (
       <div className={"result-header"}>
@@ -55,12 +52,17 @@ class ResultHeader extends Component {
         <div className={"container"}>
           {!isPlesk && this.renderConfigForm()}
           {isSpeedKitComparison && (
-            <div className="text-center" style={{ padding: '0px 16px' }}>
-              <div className="flex justify-center" style={{ alignItems: 'center' }}>
-                <h4>{isSpeedKitInstalledCorrectly(configAnalysis, isWordPress) ? (
-                  "Thank you for using Speed Kit " + (isWordPress ? '' : speedKitVersion)) : (
-                  "Thank you for installing Speed Kit, the configuration is not done yet. Please see below for more information."
-                )}</h4>
+            <div className="text-center" style={{ padding: "0px 16px" }}>
+              <div
+                className="flex justify-center"
+                style={{ alignItems: "center" }}
+              >
+                <h4>
+                  {isSpeedKitInstalledCorrectly(configAnalysis, isWordPress)
+                    ? "Thank you for using Speed Kit " +
+                      (isWordPress ? "" : speedKitVersion)
+                    : "Thank you for installing Speed Kit, the configuration is not done yet. Please see below for more information."}
+                </h4>
               </div>
             </div>
           )}
@@ -70,8 +72,8 @@ class ResultHeader extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default ResultHeader
+export default ResultHeader;
