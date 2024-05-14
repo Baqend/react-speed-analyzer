fa0f8e60a1878b31ca619649360ad777ec699fc3
Julian Schenkemeyer <julian.schenkemeyer@baqend.com>
2024-05-14 15:31:02 +0200

    chore(test.js): update formatting

--- src/actions/test.js
+++ src/actions/test.js
@@ -10,67 +10,71 @@ import {
   RESET_TEST_RESULT,
   SPEED_KIT_RESULT_NEXT,
   COMPETITOR_RESULT_NEXT,
-} from './types'
+} from "./types";
 
-import { isURL, trackURL } from '../helper/utils'
-import { stringifyObject } from '../lib/stringify-object'
+import { isURL, trackURL } from "../helper/utils";
+import { stringifyObject } from "../lib/stringify-object";
 
 /**
  * Prepares the test before its execution (check rate limiting and normalize url).
  */
 export const prepareTest = (url = null) => ({
-  'BAQEND': async ({ dispatch, getState, db }) => {
+  BAQEND: async ({ dispatch, getState, db }) => {
     dispatch({
       type: INIT_TEST,
-    })
+    });
     try {
       if (!isURL(url)) {
-        throw new Error("Input is not a valid url")
+        throw new Error("Input is not a valid url");
       }
-    } catch(e) {
+    } catch (e) {
       dispatch({
         type: RESET_TEST_RESULT,
-      })
+      });
       dispatch({
         type: ADD_ERROR,
         payload: e.message,
-      })
-      throw e
+      });
+      throw e;
     }
-  }
-})
+  },
+});
 
 /**
  * Triggers the start of a new test.
  */
 export const startTest = (useAdvancedConfig = true, isFireHorse = false) => ({
-  'BAQEND': async ({ dispatch, getState, db }) => {
+  BAQEND: async ({ dispatch, getState, db }) => {
     dispatch({
-      type: RESET_TEST_RESULT
-    })
-    const { url, location, caching, mobile, activityTimeout, cookie } = getState().config
+      type: RESET_TEST_RESULT,
+    });
+    const { url, location, caching, mobile, activityTimeout, cookie } =
+      getState().config;
     try {
       dispatch({
         type: START_TEST,
-      })
-      const { testOverview } = getState().result
+      });
+      const { testOverview } = getState().result;
 
-      const speedKit = testOverview.isSpeedKitComparison
-      const withScraping = testOverview.withScraping || false
-      let speedKitConfig = !speedKit || (speedKit && useAdvancedConfig) ? getState().config.speedKitConfig : null
+      const speedKit = testOverview.isSpeedKitComparison;
+      const withScraping = testOverview.withScraping || false;
+      let speedKitConfig =
+        !speedKit || (speedKit && useAdvancedConfig)
+          ? getState().config.speedKitConfig
+          : null;
 
       if (mobile && speedKitConfig) {
         // eslint-disable-next-line no-eval
-        const speedKitConfigObj = eval(`(${speedKitConfig})`)
+        const speedKitConfigObj = eval(`(${speedKitConfig})`);
         if (!speedKitConfigObj.userAgentDetection) {
-          speedKitConfigObj.userAgentDetection = true
+          speedKitConfigObj.userAgentDetection = true;
         }
-        speedKitConfig = stringifyObject(speedKitConfigObj, { indent: '  ' })
+        speedKitConfig = stringifyObject(speedKitConfigObj, { indent: "  " });
       }
 
-      trackURL('startComparison', url)
+      trackURL("startComparison", url);
 
-      return db.modules.post('startComparison', {
+      return db.modules.post("startComparison", {
         url,
         location,
         caching,
@@ -79,200 +83,231 @@ export const startTest = (useAdvancedConfig = true, isFireHorse = false) => ({
         activityTimeout,
         cookie,
         withScraping,
-        app: isFireHorse ? 'firehorse' : 'makefast',
+        app: isFireHorse ? "firehorse" : "makefast",
         withPuppeteer: false,
         hostname: window.location.hostname,
-      })
-    } catch(e) {
-      trackURL('failedComparison', url)
+      });
+    } catch (e) {
+      trackURL("failedComparison", url);
 
       dispatch({
         type: RESET_TEST_RESULT,
-      })
+      });
       dispatch({
         type: ADD_ERROR,
         payload: e.message,
-      })
-      throw e
+      });
+      throw e;
     }
-  }
-})
+  },
+});
 
 /**
  * Checks the status of a given test and subscribes to the result.
  * @param testId The id of the test to be monitored.
  */
 export const monitorTest = (testId, onAfterFinish) => ({
-  'BAQEND': async ({ dispatch }) => {
-    dispatch({ type: MONITOR_TEST })
-    dispatch(computeTestProgress())
-    const testOverview = await dispatch(subscribeToTestOverview({ testId, onAfterFinish }))
+  BAQEND: async ({ dispatch }) => {
+    dispatch({ type: MONITOR_TEST });
+    dispatch(computeTestProgress());
+    const testOverview = await dispatch(
+      subscribeToTestOverview({ testId, onAfterFinish })
+    );
     // TODO: This is needed for the status update of the embedded version
     if (!testOverview.hasFinished) {
-      dispatch(checkTestStatus())
+      dispatch(checkTestStatus());
     }
-  }
-})
+  },
+});
 
 const subscribeToTestResult = (testId, isSpeedKitComparison) => ({
-  'BAQEND': async ({ dispatch, getState, db }) => {
-    const stream = db.TestResult.find().equal('id', testId).resultStream()
+  BAQEND: async ({ dispatch, getState, db }) => {
+    const stream = db.TestResult.find().equal("id", testId).resultStream();
     const subscription = stream.subscribe((res) => {
-      const testResult = res[0] ? res[0].toJSON() : null
+      const testResult = res[0] ? res[0].toJSON() : null;
       if (!testResult) {
-        return
+        return;
       }
 
       dispatch({
-        type: isSpeedKitComparison ? SPEED_KIT_RESULT_NEXT: COMPETITOR_RESULT_NEXT,
-        payload: testResult
-      })
+        type: isSpeedKitComparison
+          ? SPEED_KIT_RESULT_NEXT
+          : COMPETITOR_RESULT_NEXT,
+        payload: testResult,
+      });
 
       if (testResult.hasFinished) {
-        subscription.unsubscribe()
+        subscription.unsubscribe();
       }
-    })
-  }
-})
+    });
+  },
+});
 
 const subscribeToTestOverview = ({ testId, onAfterFinish }) => ({
-  'BAQEND': async ({ dispatch, getState, db }) => {
-    let isResolved = false
-    let trackUnload = null
-    const testOverviewStream = db.TestOverview.find().equal('id', `/db/TestOverview/${testId}`).resultStream()
+  BAQEND: async ({ dispatch, getState, db }) => {
+    let isResolved = false;
+    let trackUnload = null;
+    const testOverviewStream = db.TestOverview.find()
+      .equal("id", `/db/TestOverview/${testId}`)
+      .resultStream();
     const testOverviewPromise = new Promise((resolve, reject) => {
       const testOverviewSubscription = testOverviewStream.subscribe((res) => {
-        const testOverview = res[0] ? res[0].toJSON() : null
+        const testOverview = res[0] ? res[0].toJSON() : null;
         if (testOverview) {
-          if (testOverview.competitorTestResult && !getState().result.speedKitTestResult) {
-            dispatch(subscribeToTestResult(testOverview.competitorTestResult, false))
+          if (
+            testOverview.competitorTestResult &&
+            !getState().result.speedKitTestResult
+          ) {
+            dispatch(
+              subscribeToTestResult(testOverview.competitorTestResult, false)
+            );
           }
-          if (testOverview.speedKitTestResult && !getState().result.speedKitTestResult) {
-            dispatch(subscribeToTestResult(testOverview.speedKitTestResult, true))
+          if (
+            testOverview.speedKitTestResult &&
+            !getState().result.speedKitTestResult
+          ) {
+            dispatch(
+              subscribeToTestResult(testOverview.speedKitTestResult, true)
+            );
           }
 
           if (!trackUnload) {
             trackUnload = () => {
-              trackURL('leaveDuringTest', testOverview.url, { startTime: getState().result.startTime })
-            }
-            window.addEventListener('beforeunload', trackUnload)
+              trackURL("leaveDuringTest", testOverview.url, {
+                startTime: getState().result.startTime,
+              });
+            };
+            window.addEventListener("beforeunload", trackUnload);
           }
 
           if (testOverview.hasFinished) {
-            window.removeEventListener('beforeunload', trackUnload)
-            testOverviewSubscription && testOverviewSubscription.unsubscribe()
+            window.removeEventListener("beforeunload", trackUnload);
+            testOverviewSubscription && testOverviewSubscription.unsubscribe();
             // Do not trigger reload if the test is not started anymore e.g. because of backward navigation
-            onAfterFinish && getState().result.isStarted && onAfterFinish({ testId })
+            onAfterFinish &&
+              getState().result.isStarted &&
+              onAfterFinish({ testId });
           }
           if (isResolved) {
             dispatch({
               type: TESTOVERVIEW_NEXT,
-              payload: testOverview
-            })
+              payload: testOverview,
+            });
           } else {
             dispatch({
               type: TESTOVERVIEW_LOAD,
-              payload: testOverview
-            })
+              payload: testOverview,
+            });
           }
           testOverviewPromise.then(() => {
-            isResolved = true
-          })
-          resolve(testOverview)
+            isResolved = true;
+          });
+          resolve(testOverview);
         }
-      })
-    })
-    return testOverviewPromise
-  }
-})
+      });
+    });
+    return testOverviewPromise;
+  },
+});
 
 const computeTestProgress = () => ({
-  'BAQEND': ({ dispatch, getState }) => {
-    const interval = setInterval( () => {
+  BAQEND: ({ dispatch, getState }) => {
+    const interval = setInterval(() => {
       const doComputation = (upperBorder) => {
-        const persistedProgress = parseInt(sessionStorage.getItem('baqend-progress') || 0, 10)
-        const { testProgress: reduxProgress } = getState().result
-        const testProgress = persistedProgress < 100 ? persistedProgress : reduxProgress
-        const increaseBy = Math.floor(Math.random() * 3) + 1
+        const persistedProgress = parseInt(
+          sessionStorage.getItem("baqend-progress") || 0,
+          10
+        );
+        const { testProgress: reduxProgress } = getState().result;
+        const testProgress =
+          persistedProgress < 100 ? persistedProgress : reduxProgress;
+        const increaseBy = Math.floor(Math.random() * 3) + 1;
 
         // Set immediately to 100% if the upper border is 100 (test has finished).
-        const computedProgress = upperBorder < 100 ? testProgress + increaseBy : 100
-        const newProgress = computedProgress < upperBorder ? computedProgress : upperBorder
+        const computedProgress =
+          upperBorder < 100 ? testProgress + increaseBy : 100;
+        const newProgress =
+          computedProgress < upperBorder ? computedProgress : upperBorder;
         dispatch({
           type: UPDATE_TEST_PROGRESS,
-          payload: newProgress
-        })
-      }
+          payload: newProgress,
+        });
+      };
 
       const getUpperBorder = () => {
-        const { testOverview, speedKitTest, competitorTest } = getState().result
-        const { hasFinished: testOverviewFinished } = testOverview
-        const { hasFinished: speedKitFinished, webPagetests } = speedKitTest
-        const { hasFinished: competitorFinished } = competitorTest
+        const { testOverview, speedKitTest, competitorTest } =
+          getState().result;
+        const { hasFinished: testOverviewFinished } = testOverview;
+        const { hasFinished: speedKitFinished, webPagetests } = speedKitTest;
+        const { hasFinished: competitorFinished } = competitorTest;
 
         // Check if the whole test (both speed kit and competitor) is finished.
         // Check first to ensure that the progress bar always finish with 100%.
         if (testOverviewFinished) {
-          return 100
+          return 100;
         }
 
         // Check if the puppeteer analysis is finished.
         if (!competitorFinished) {
-          return 50
+          return 50;
         }
 
         // Check if the prewarm run of the speed kit test is finished.
-        if (!webPagetests || !webPagetests.find((test) => test.testType === 'prewarm' && test.status === 'SUCCESS')) {
-          return 75
+        if (
+          !webPagetests ||
+          !webPagetests.find(
+            (test) => test.testType === "prewarm" && test.status === "SUCCESS"
+          )
+        ) {
+          return 75;
         }
 
         // Check if at least one of the two test is finished.
         if (!speedKitFinished && !competitorFinished) {
-          return 90
+          return 90;
         }
 
-        return 99
-      }
+        return 99;
+      };
 
-      const upperBorder = getUpperBorder()
-      doComputation(upperBorder)
-      upperBorder === 100 && clearInterval(interval)
-    }, 750)
-    return interval
-  }
-})
+      const upperBorder = getUpperBorder();
+      doComputation(upperBorder);
+      upperBorder === 100 && clearInterval(interval);
+    }, 750);
+    return interval;
+  },
+});
 
 const checkTestStatus = () => ({
-  'BAQEND': ({ dispatch, getState, db }) => {
+  BAQEND: ({ dispatch, getState, db }) => {
     const pullTestStatus = (id) => ({
-      'BAQEND': async ({ dispatch, db }) => {
-        const res = await db.modules.get('getTestStatus', { baqendId: id })
+      BAQEND: async ({ dispatch, db }) => {
+        const res = await db.modules.get("getTestStatus", { baqendId: id });
         const status = {
           statusCode: res.status.statusCode,
-          statusText: res.status.statusText
-        }
+          statusText: res.status.statusText,
+        };
         dispatch({
           type: TEST_STATUS_GET,
-          payload: status
-        })
-        return status
-      }
-    })
+          payload: status,
+        });
+        return status;
+      },
+    });
     const interval = setInterval(async () => {
-      const { testOverview } = getState().result
-      const testId = testOverview.competitorTestResult
+      const { testOverview } = getState().result;
+      const testId = testOverview.competitorTestResult;
       if (testId) {
         try {
-          const { statusCode } = await dispatch(pullTestStatus(testId))
+          const { statusCode } = await dispatch(pullTestStatus(testId));
           if (statusCode === 100 || statusCode === 200) {
-            clearInterval(interval)
+            clearInterval(interval);
           }
         } catch (e) {
-          clearInterval(interval)
+          clearInterval(interval);
         }
       }
-    }, 2000)
-    return interval
-  }
-})
-
+    }, 2000);
+    return interval;
+  },
+});
